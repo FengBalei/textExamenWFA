f5985310df83bd82b8e09fc4c997c239f2df2a1f
CharlotteVanParys <vanparyscharlotte@hotmail.com>
2023-01-11 09:11:20 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+window.addEventListener('load',initialize);
+
+let slcTeams;
+let divShirt;
+let divRiders;
+let secDetail;
+let secOrderbox;
+let cyclingData = [];
+let currentTeam;
+
+async function initialize(){
+  
+}
+
+function bindElements(){
+   
+}
+
+function addEventListeners(){
+  
+}
+
+function fillSlcTeams(){
+  
+}
